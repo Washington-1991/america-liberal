ee4be5dde8a4458dddbe259dde8d62f9f4f9928f
Washington Arambillete <milanghdarkoh@hotmail.com>
2024-06-28 21:21:27 +0200

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :sessions, only: [:new, :create, :destroy]
-  get 'session_new', to: 'sessions#session_new'
 
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
