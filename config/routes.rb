ad767f3b8511f9825a6ad489bc221f6cb7c090da
Washington Arambillete <milanghdarkoh@hotmail.com>
2024-07-26 22:48:10 +0200

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "eeuu", to: "pages#eeuu"
   get 'argentina', to: 'pages#argentina'
   get 'uruguay', to: 'pages#uruguay'
   get "home", to: "pages#home"
   get "financiacion", to: "pages#financiacion"
-
-
-
 
   # Defines the root path route ("/")
   # root "posts#index"
