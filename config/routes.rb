e052ae4687d4e92d5bb99b948cbd61c4db8c133e
Washington Arambillete <milanghdarkoh@hotmail.com>
2024-07-28 22:42:13 +0200

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "eeuu", to: "pages#eeuu"
   get 'argentina', to: 'pages#argentina'
+  get 'brasil', to: 'pages#brasil'
+  get "eeuu", to: "pages#eeuu"
   get 'uruguay', to: 'pages#uruguay'
   get "home", to: "pages#home"
   get "financiacion", to: "pages#financiacion"
